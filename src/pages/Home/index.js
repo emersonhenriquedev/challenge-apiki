942ab4eeb66e062d3b810cf90b2d0bf506d277d5
Emerson Henrique <emerson.hm@outlook.com>
2021-07-12 13:33:29 -0300

    updated readme

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,15 +12,23 @@ export default function Home() {
   const [loading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPage, setTotalPage] = useState(undefined)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     async function fetchPosts() {
-      const response = await axios.get((`https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518&page=${currentPage}`))
-      setPosts([...posts, ...response.data])
-      setLoading(false)
+      try {
+        const response = await axios.get((`https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518&page=${currentPage}`))
+        setPosts([...posts, ...response.data])
+        setLoading(false)
 
-      if (currentPage === 1) {
-        setTotalPage(parseInt(response.headers['x-wp-totalpages']))
+        if (currentPage === 1) {
+          setTotalPage(parseInt(response.headers['x-wp-totalpages']))
+        }
+
+      } catch (err) {
+        setError(true)
+        console.log('Houve um erro na requisição')
+        console.log(err)
       }
     }
 
@@ -41,6 +49,17 @@ export default function Home() {
       return <button className="posts__btn" onClick={handleLoadMoreButton}>Carregar mais</button>
     }
     return <p>Não há mais posts para exibir</p>
+  }
+
+  if (error) {
+    return (
+      <div className="posts ">
+        
+        <div style={{ textAlign: 'center' }}>
+          <h1>A página não pôde ser exibida</h1>  
+        </div>
+      </div>
+    )
   }
 
   return (
